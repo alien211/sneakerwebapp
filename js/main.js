634e82476b0513f7cb3fbd731e80eaba6776d704
Marcell Ilyés <ilyesmarci@gmail.com>
2021-12-10 03:49:57 +0100

    Finished product list page with working filters

--- js/main.js
+++ js/main.js
@@ -17,8 +17,13 @@ function myFunction() {
 /* ------- PRODUCT LIST page ------- */
 
 let _products = [];
+let _brands = [];
 
+<<<<<<< Updated upstream
 // fetch data from the artists json file by MI
+=======
+// fetch data from the sneakers json file
+>>>>>>> Stashed changes
 
 async function fetchData() {
     const response = await fetch('json/sneakers.json');
@@ -26,6 +31,13 @@ async function fetchData() {
     _products = data;
     console.log(_products);
     appendProducts(_products);
+
+    const brands = _products.map(product => product.brand);
+    console.log(brands);
+
+    _brands = [...new Set(brands)]
+    console.log(_brands);
+    appendBrands(_brands);
 }
 
 fetchData();
@@ -41,10 +53,56 @@ function appendProducts(products) {
         <div>
         <h4>${product.name}</h4>
         <h3>${product.color}</h3>
-        <p>Lowest Ask<br>$${product.price}</p>
+        <h5>Lowest Ask</h5>
+        <p>$${product.price}</p>
         </div>
         </article>
     `;
     }
     document.querySelector('#gridProducts').innerHTML = htmlTemplate;
+}
+
+// sorting functions
+
+function orderBy(option) {
+    if (option === "name") {
+      orderByName();
+    } else if (option === "price") {
+      orderByPrice();
+    } else if (option === "brand") {
+      orderByBrand();
+    }
+  }
+
+  function orderByName() {
+    _products.sort((product1, product2) => {
+        return product1.name.localeCompare(product2.name);
+    });
+    appendProducts(_products);
+}
+
+function orderByPrice() {
+    _products.sort((product1, product2) => {
+        return product1.price.localeCompare(product2.price);
+    });
+    appendProducts(_products);
+}
+
+// append all brands as select options (dropdown)
+
+function appendBrands(brands) {
+    let htmlTemplate = "";
+    for (let brand of brands) {
+        htmlTemplate += /*html*/`
+        <option value="${brand}">${brand}</option>
+    `;
+    }
+    document.querySelector('#sortByBrand').innerHTML += htmlTemplate;
+}
+
+// filter products by selected brand
+
+function filterByBrand(brand) {
+    const results = _products.filter(product => product.brand === brand);
+    appendProducts(results);
 }
