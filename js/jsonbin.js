b47539539b2bc78e0b58a5ddeb22ae4b3798c488
NProlic <eaanipr@students.eaaa.dk>
2021-12-14 02:44:15 +0100

    Barely functional create and delete options

--- js/jsonbin.js
+++ js/jsonbin.js
@@ -26,7 +26,7 @@ async function loadSneakers() {
     _sneakers = data;
     appendSneakers(_sneakers);
 }
-//loadSneakers();
+loadSneakers();
 
 /**
  * Appends users to the DOM
@@ -36,13 +36,14 @@ function appendSneakers(sneakers) {
     let htmlTemplate = "";
     for (const sneaker of sneakers) {
       htmlTemplate += /*html*/ `
-        <article onclick="showDetailedPage(${sneaker.id})">
-        <img src="${sneaker.img_url}">
+        <article>
+        <img src="${sneaker.img_url}" onclick="showDetailedPage(${sneaker.id})">
             <div>
                 <h4>${sneaker.name}</h4>
                 <h3>${sneaker.color}</h3>
                 <h5>Lowest Ask</h5>
                 <p>$${sneaker.price}</p>
+                <button onclick="deleteSenakers(${sneaker.id})">Delete</button>
             </div>
         </article>
         `;
@@ -50,4 +51,71 @@ function appendSneakers(sneakers) {
     document.querySelector("#gridSneakers").innerHTML = htmlTemplate;
     //showLoader(false);
   }
+
+  // ========== CREATE ==========
+
+/**
+ * Creates a new user with properties: name, mail & id
+ */
+async function createSale() {
+  // references to input fields
+  let nameInput = document.querySelector("#name");
+  let brandInput = document.querySelector("#brand");
+  let colorInput = document.querySelector("#color");
+  let priceInput = document.querySelector("#price");
+  let imgInput = document.querySelector("#img");
+  // dummy generated user id
+  const sneakerId = Date.now();
+  // declaring a new user object
+  const newSneakers = {
+    name: nameInput.value,
+    brand: brandInput.value,
+    color: colorInput.value,
+    price: priceInput.value,
+    img: imgInput.value,
+    id: sneakerId
+  };
+  // pushing the new user object to the _users array
+  _sneakers.push(newSneakers);
+  // wait for update
+  await updateJSONBIN(_sneakers);
+  // reset
+  nameInput.value = "";
+  brandInput.value = "";
+  colorInput.value = "";
+  priceInput.value = "";
+  imgInput.value = "";
+  //navigating back
+  navigateTo("#/");
+}
+
+// ========== DELETE ==========
+/**
+ * Deletes user by given user id
+ * @param id 
+ */
+ async function deleteSenakers(id) {
+  _sneakers = _sneakers.filter(sneaker => sneaker.id !== id);
+  await updateJSONBIN(_sneakers);
+  navigateTo("#/my-account");
+}
+
+// ========== Services ==========
+/**
+ * Updates the data source on jsonbin with a given users arrays
+ * @param {Array} sneakers 
+ */
+ async function updateJSONBIN(sneakers) {
+  // put users array to jsonbin
+  const response = await fetch(_baseUrl, {
+    method: "PUT",
+    headers: _headers,
+    body: JSON.stringify(sneakers)
+  });
+  // waiting for the result
+  const result = await response.json(); // the new updated users array from jsonbin
+  console.log(result);
+  //updating the DOM with the new fetched users
+  appendSneakers(result.record);
+}
   
